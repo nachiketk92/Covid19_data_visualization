239174fdf86bb01fb1f9155df1ae6e3af7798184
nachiketkale <achiket.kale@mail.mcgill.ca>
2020-04-15 18:00:47 -0400

    Data scraping for worldometer site using BeautifulSoup Next step: Study the HTML and get the clean data from scraping

--- covie_data_scrapping.py
+++ covie_data_scrapping.py
@@ -5,4 +5,40 @@ Created on Tue Apr 14 16:06:44 2020
 
 @author: nachiketkale
 """
+import requests
+import csv
+from bs4 import BeautifulSoup
+#MAke a get request to url
+url=requests.get("https://www.worldometers.info/coronavirus/")
+# checking the status  
+print (url.status_code)
+#
+src=url.content
 
+#html.parser is a type of parser we can use different parsers once we know the difference between them
+soup=BeautifulSoup(src,'html.parser')
+#creating new csv file
+filename='Covid_data_worldometer.csv'
+
+#create new variable of csv writer
+csv_writer=csv.writer(open(filename,'w'))
+
+#run a for loop to extract table data and save it in csv
+for tr in soup.find_all('tr'):
+    data=[]
+    #for extracting the table heading
+    for th in tr.find_all('th'):
+        data.append(th.text)
+        
+    if(data):
+        print("inserting headers :{}".format(','.join(data)))
+        csv_writer.writerow(data)
+        continue
+    
+    #scraping actual table values
+    for td in tr.find_all('td'):
+        data.append(td.text.strip())
+    if (data):
+        print("inserting table data: {}".format(','.join(data)))
+        csv_writer.writerow(data)
+    
