48826ad79b2540ae703d85e596004990cf27cfc3
nachiketkale <achiket.kale@mail.mcgill.ca>
2020-04-17 00:36:46 -0400

    Problem with data duplicacy in data scrapping is fixed Basic initial structure of how we want to proceed with visualization is introduced

--- covie_data_scrapping.py
+++ covie_data_scrapping.py
@@ -12,11 +12,9 @@ from bs4 import BeautifulSoup
 url=requests.get("https://www.worldometers.info/coronavirus/")
 # checking the status  
 print (url.status_code)
-#
-src=url.content
-
 #html.parser is a type of parser we can use different parsers once we know the difference between them
-soup=BeautifulSoup(src,'html.parser')
+soup=BeautifulSoup(url.content,'html.parser')
+table=soup.find('table',id="main_table_countries_today")
 #creating new csv file
 filename='Covid_data_worldometer.csv'
 
@@ -24,7 +22,7 @@ filename='Covid_data_worldometer.csv'
 csv_writer=csv.writer(open(filename,'w'))
 
 #run a for loop to extract table data and save it in csv
-for tr in soup.find_all('tr'):
+for tr in table.find_all('tr'):
     data=[]
     #for extracting the table heading
     for th in tr.find_all('th'):
@@ -38,7 +36,11 @@ for tr in soup.find_all('tr'):
     #scraping actual table values
     for td in tr.find_all('td'):
         data.append(td.text.strip())
+    print (data)
     if (data):
         print("inserting table data: {}".format(','.join(data)))
         csv_writer.writerow(data)
+        
+    
+    
     
