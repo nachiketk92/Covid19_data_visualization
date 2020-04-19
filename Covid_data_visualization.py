503e2ecce1723f15514b8919f51232c3b6567958
nachiketkale <achiket.kale@mail.mcgill.ca>
2020-04-19 15:40:12 -0400

    Modified code for data scrapping which saves more refined and clean data to csv Also the csv filne name is updated by providing it a data stamp.

--- Covid_data_visualization.py
+++ Covid_data_visualization.py
@@ -12,15 +12,10 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("Covid_data_worldometer.csv")
 
 # sample slicing and plot for one country 
-# next st
-
+#next step creating a plots class with different methods for different plots
 country_data=df.iloc[8,1:7].replace(',','',regex=True).astype(float)
 # pie chart for single country
 plt.pie(country_data, autopct='%1.1f%%', shadow=True, startangle=240)
 plt.title("Percentage chart")
 plt.show()
-#pie chart for world and continent 
-World_and_continent_data=df.iloc[7,1:7].replace(',','',regex=True).astype(float)
-plt.pie(World_and_continent_data, autopct='%1.1f%%', shadow=True, startangle=240)
-plt.title("Percentage chart")
-plt.show()
+ 
--- covie_data_scrapping.py
+++ covie_data_scrapping.py
@@ -8,6 +8,9 @@ Created on Tue Apr 14 16:06:44 2020
 import requests
 import csv
 from bs4 import BeautifulSoup
+import pandas as pd
+from  datetime import datetime
+
 #MAke a get request to url
 url=requests.get("https://www.worldometers.info/coronavirus/")
 # checking the status  
@@ -15,32 +18,31 @@ print (url.status_code)
 #html.parser is a type of parser we can use different parsers once we know the difference between them
 soup=BeautifulSoup(url.content,'html.parser')
 table=soup.find('table',id="main_table_countries_today")
-#creating new csv file
-filename='Covid_data_worldometer.csv'
+get_table_data=table.tbody.find_all("tr")
+#creatign  the dictionary to store key value data
+dic={}
+#extracting ketys of the dict
+for i in range(len(get_table_data)):
+    try:
+        key=get_table_data[i].find_all("a",href=True)[0].string
+    except:
+        key=get_table_data[i].find_all("td")[0].string
+#extracting values for tkeys        
+    values=[j.string for j in get_table_data[i].find_all("td")]
 
-#create new variable of csv writer
-csv_writer=csv.writer(open(filename,'w'))
+    dic[key]=values
 
-#run a for loop to extract table data and save it in csv
-for tr in table.find_all('tr'):
-    data=[]
-    #for extracting the table heading
-    for th in tr.find_all('th'):
-        data.append(th.text)
-        
-    if(data):
-        print("inserting headers :{}".format(','.join(data)))
-        csv_writer.writerow(data)
-        continue
-    
-    #scraping actual table values
-    for td in tr.find_all('td'):
-        data.append(td.text.strip())
-    print (data)
-    if (data):
-        print("inserting table data: {}".format(','.join(data)))
-        csv_writer.writerow(data)
-        
+column_names=["Total Cases","New Cases","Total Deaths", "New Deaths","Total Recovered","Active Cases","Serious,Critical","Total Cases/1M people","Total Death/1M people","Total Tests","Tests/1M people","Continent"]
+
+df=pd.DataFrame(dic).iloc[1:,:].T
+
+df.index_name="Country"
+df.columns=column_names
+
+df.to_csv(datetime.now().strftime('covid_data_worldometer-%Y-%m-%d.csv'))
+
+
+
     
     
     
